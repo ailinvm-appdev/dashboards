2ab41aee1be16ac0e7f937c352dc56c7d212f3b1
ailinvm <ailinvm0@chicagobooth.edu>
2022-07-12 01:49:24 +0000

    100 but fake

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -9,4 +9,27 @@ class CurrenciesController < ApplicationController
 
   render({ :template => "currency_templates/step_one.html.erb"})
   end
+
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+ 
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+    @from_currency = params.fetch("from_currency")
+    render({ :template => "currency_templates/step_two.html.erb"})
+
+  end 
+
+  def third_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+ 
+    @symbols_hash = @parsed_data.fetch("symbols")
+    @array_of_symbols = @symbols_hash.keys
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+    render({ :template => "currency_templates/step_three.html.erb"})
+
+  end 
 end
